31258cf1eda4e4bea10f008ef753e24cb72789f9
dopre-creator <227175624+mariajulia30062008-dotcom@users.noreply.github.com>
2026-03-23 11:57:51 -0300

    RPG23.03

--- projeto.rpg/jogo.js
+++ projeto.rpg/jogo.js
@@ -8,12 +8,37 @@ class Personagem {
         this.energia = energia;
     }
     atacar(alvo, habilidade) {
-        
+        // verificar se tem mana/energia
+        if (this.mana >= habilidade.custo
+            && this.energia >= habilidade.energia) {
+                alvo.hp -= habilidade.dano;
+                //Debitar mana
+                if (habilidade.custo > 0) {
+                    this.mana -= habilidade.custo;
+                    this.energia += 50;
+                }
+                //esvaziar energia
+                if (habilidade.energia > 0) {
+                    this.energia = 0;
+                }
+                return `${this.nome} usou ${habilidade.nome}`;
+        } else {
+            return `Sem mana para usar ${habilidade.nome}`;
+        }
+
+    }
+    boss_atacar(alvo){
+        if (this.energia == 100){
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else{
+            this.energia += 50;
+        }
     }
 }
 
-class Habilidade{
-    constructor(id, nome, dano, custo, energia){
+class Habilidade {
+    constructor(id, nome, dano, custo, energia) {
         this.id = id;
         this.nome = nome;
         this.dano = dano;
@@ -29,7 +54,15 @@ let boss = new Personagem("Water demon", "Boss", 100, 0, 50);
 document.getElementById("nome-heroina").textContent = `${hero.nome}`;
 document.getElementById("titulo-heroina").textContent = `✨⚔️${hero.titulo}`;
 document.getElementById("nome-boss").textContent = `${boss.nome}`;
-document.getElementById("titulo-boss").textContent= `👹💧${boss.titulo}`;
+document.getElementById("titulo-boss").textContent = `👹💧${boss.titulo}`;
+// atualizar as barras de vida
+const atualizarInterface = (mensagem) => {
+    document.getElementById("hp-hero").value = hero.hp;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("en-hero").value = hero.energia;
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("en-boss").value = boss.energia;
+}
 
 let container = document.getElementById("controles");
 let listaHabilidades = [
@@ -42,7 +75,14 @@ listaHabilidades.forEach(hab => {
     btn.innerText = hab.nome;
     btn.classList.add("btn", "btn-info");
     container.appendChild(btn);
-}) 
+    btn.onclick = () => {
+        let mensagem = hero.atacar(boss, hab);
+        atualizarInterface(mensagem);
+        boss.boss_atacar(hero);
+    }
+});
+
+
 
 
 
