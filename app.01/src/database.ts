fb8466fe8734f36f45ea43aa2c2aba7e45ef47fa
dopre-creator <mariajulia.30062008@gmail.com>
2026-03-27 12:13:49 -0300

    App01

---
+++ app.01/src/database.ts
@@ -0,0 +1,18 @@
+import { Livro } from "livro";
+export const  biblioteca : Livro[] = [
+    {
+    id: 1, 
+    titulo: "Dom Casmurro", 
+    autor: "Machado de Assis",
+    ano: 1899,
+    disponivel :true
+    },
+    {
+    id: 2, 
+    titulo: "Olhos d' Água", 
+    autor: "Conceição Evaristo",
+    ano: 2014,
+    disponivel: false,
+    categoria: "Ficção"
+    }
+] 
